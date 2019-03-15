50f70ef55443427d0d264b6dad15cde7b69e4ae7
Gary Fagan <garyfagan1987@googlemail.com>
2019-03-15 16:54:45 +0000

    updating token for poc

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -13,7 +13,7 @@ import createStore from '../store';
 const { Header, Content, Footer } = Layout;
 
 // @todo, this token needs to be set when authenticated
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzNiMTUwNzhhZDUxOTdkYTZjNjkyNzgiLCJpYXQiOjE1NDczNzU4Nzl9.ABBMgXk00ud4oRbvCHWYwHLzJUITN1mTDIF_B997Rk4';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YzNhMzJjNzhmYjJkNTQ5MzFjNjhmZmEiLCJlbWFpbCI6ImdhcnlmYWdhbjE5ODdAZ29vZ2xlbWFpbC5jb20iLCJuYW1lIjoiR2FyeSBGYWdhbiIsImlhdCI6MTU1MjY2ODgwMn0.Vwp_KM2RYLroJ9aSWVOCBYkRSfaqBEVfRHWx0LPi3kw';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
