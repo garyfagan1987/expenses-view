547a85bd7d38eb6d77b8a86b80f09ce20016f771
Gary Fagan <garyfagan1987@googlemail.com>
2019-06-14 08:02:14 +0100

    fix the decimal range

--- helpers/parsers.js
+++ helpers/parsers.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const currency = (amount = 0) => `£${amount}`;
+export const currency = (amount = 0) => `£${parseFloat(amount).toFixed(2)}`;
