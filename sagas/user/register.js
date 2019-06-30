68176d807e20dc027dfcdf4fcdb9852ff5ca6268
Gary Fagan <garyfagan1987@googlemail.com>
2019-06-30 15:52:04 +0100

    login when the user registers

--- sagas/user/register.js
+++ sagas/user/register.js
@@ -1,9 +1,10 @@
 import { put } from 'redux-saga/effects';
 import { message } from 'antd';
 
-import { userRegisterError, userRegisterSuccess } from '../../actions/user/register';
+import { userRegisterError } from '../../actions/user/register';
 import { registerPath } from '../../config/endpoints';
 import messages from '../../config/messages';
+import userAuthenticate from './authenticate';
 
 export default function* userRegister({ payload: values }) {
   try {
@@ -18,8 +19,12 @@ export default function* userRegister({ payload: values }) {
       message.error(messages.register.error);
       throw new Error(messages.register.error);
     }
-    message.success(messages.register.success);
-    yield put(userRegisterSuccess());
+    yield* (userAuthenticate({
+      payload: {
+        email: values.email,
+        password: values.password,
+      },
+    }));
   } catch (err) {
     yield put(userRegisterError());
   }
